6706058a05a54a6cde064aaf89cdc31ba0d672b3
sujalraikar2004 <sujalraikar84@gmail.com>
2025-10-26 01:10:03 +0530

    hhh

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 
 const app=express()
 app.use(cors({
-  origin: 'https://signaturedrapes.in',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
